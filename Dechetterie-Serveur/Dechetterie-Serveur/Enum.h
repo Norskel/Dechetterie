4668f19864f781c6a658c4f863e6e9b1ba54a175
Martin KLEIN-NORTH <Martin KLEIN-NORTH@DESKTOP-R8VJ3IR>
2018-04-24 05:06:41 +0200

    update

--- Dechetterie-Serveur/Dechetterie-Serveur/Enum.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-static public enum class id_groupe : char { nonDef, Entrée, Sortie};
-static public enum class id_client : char { nonDef, ClientBarrière, ClientBalance, ClientRFID};
-
-
